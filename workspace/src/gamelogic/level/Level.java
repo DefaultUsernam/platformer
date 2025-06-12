a7eb5da5e82005755aa94cb80e77da10cea1be1a
DefaultUsernam <121245757+DefaultUsernam@users.noreply.github.com>
2025-06-12 13:19:03 +0000

    -commit levels added

--- workspace/src/gamelogic/level/Level.java
+++ workspace/src/gamelogic/level/Level.java
@@ -204,7 +204,7 @@ public class Level {
 					if(flowers.get(i).getType() == 1)
 						water(flowers.get(i).getCol(), flowers.get(i).getRow(), map, 3);
 					else
-						addGas(flowers.get(i).getCol(), flowers.get(i).getRow(), map, 20, new ArrayList<Gas>());
+						addGas(flowers.get(i).getCol(), flowers.get(i).getRow(), map, 8, new ArrayList<Gas>());
 					flowers.remove(i);
 					i--;
 				}
